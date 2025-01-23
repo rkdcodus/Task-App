73e8e8064b68a645cc91648958ade0f6c5ead938
rkdcodus <rcd7275@naver.com>
2025-01-23 17:51:25 +0900

    feat: log 모달 구현

--- src/components/LoggerModal/LoggerModal.tsx
+++ src/components/LoggerModal/LoggerModal.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { useTypedSelector } from "../../hooks/redux";
+import { FiX } from "react-icons/fi";
+import LogItem from "./LogItem/LogItem";
+import { body, closeButton, header, modalWindow, title, wrapper } from "./LoggerModal.css";
 
-const LoggerModal = () => {
-  return <div>LoggerModal</div>;
+type TLoggerModalProps = {
+  setIsLoggerOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const LoggerModal = ({ setIsLoggerOpen }: TLoggerModalProps) => {
+  const logs = useTypedSelector((state) => state.logger.logArray);
+
+  return (
+    <div className={wrapper}>
+      <div className={modalWindow}>
+        <div className={header}>
+          <div className={title}>활동 기록</div>
+          <FiX className={closeButton} onClick={() => setIsLoggerOpen(false)} />
+        </div>
+        <div className={body}>
+          {logs.map((log) => (
+            <LogItem key={log.logId} LogItem={log} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoggerModal;
