908dcf92b37777308d699d6e89affbe5478367c5
rkdcodus <rcd7275@naver.com>
2025-01-23 22:39:46 +0900

    fix: firebase API key 숨기기 취소

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyBPLMSeWnia6nQxxeJNVZZcEoq0aelLlbE",
+  authDomain: "react-task-app-843b8.firebaseapp.com",
+  projectId: "react-task-app-843b8",
+  storageBucket: "react-task-app-843b8.firebasestorage.app",
+  messagingSenderId: "587733457238",
+  appId: "1:587733457238:web:3fffe4edd40d7857355ef7",
 };
 
 // Initialize Firebase
